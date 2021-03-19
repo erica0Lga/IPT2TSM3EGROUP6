ec446fda1db4db7835d94fbb794262a9ffdf9a4b
Silastre1015 <80072043+Silastre1015@users.noreply.github.com>
2021-03-19 20:09:16 +0800

    Add files via upload

---
+++ Silastre.java
@@ -0,0 +1,22 @@
+package StudentInfo;
+
+public class function {
+
+Private string name3, addres3;cours3;
+Private int age1;
+
+public  String NameJan3() {
+   return name1 ="Jan Guiller Silastre";    
+}
+
+public String addressJan(){
+return address3= "San Rafael Fiesta Community";
+}
+
+public String courseJan(){
+return course3= "BS Informmation Technolgy";
+}
+
+public int ageJan(){ 
+return age3= 21;
+}
