a63579d38a818e5785d34a0cd74a3bb0e118e501
aeritherie <dynelyka@gmail.com>
2021-03-19 13:37:24 +0800

    adding four methods dynel fajardo

--- function.java
+++ function.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package studentinfo;
 
-/**
- *
- * @author Aerith.Kayle
- */
 public class function {
+    
+    private String name1, address1,course1;
+    private int age1;
+
+public  String NameDynel() {
+   return name1 ="Dynel Angelo Fajardo";    
+}
+
+public String addressDynel(){
+return address1= "San Isidro, Tarlac City";
+}
+
+public String courseDynel(){
+return course1= "BS Information Technology";
+}
+
+public int ageDynel(){ 
+return age1= 23;
+}
     
 }
