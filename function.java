47e28e725eff41b912d3d2e4d833363df6665e9c
Joshua Guiwa <79268739+Djosh02@users.noreply.github.com>
2021-03-19 21:05:52 +0800

    Add files via upload

--- function.java
+++ function.java
@@ -1,25 +1,33 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package studentinfo;
-
+/**
+ *
+ * @author My
+ */
 public class function {
     
-    private String name1, address1,course1;
-    private int age1;
+    private String name4, address4,course4;
+    private int age4;
 
-public  String NameDynel() {
-   return name1 ="Dynel Angelo Fajardo";    
+public  String NameJoshua() {
+   return name4 ="Joshua Guiwa";    
 }
 
-public String addressDynel(){
-return address1= "San Isidro, Tarlac City";
+public String addressJoshua(){
+return address4= "San Pascual, Tarlac City";
 }
 
-public String courseDynel(){
-return course1= "BS Information Technology";
+public String courseJoshua(){
+return course4= "BS Information Technology";
 }
 
-public int ageDynel(){ 
-return age1= 23;
+public int ageJoshua(){ 
+return age4= 22;
 }
     
-}
+} 
+
